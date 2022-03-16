14b0a784131cd44a984ee45d72c807fe203a7bba
jaehanNO <ssont86@gmail.com>
2022-03-16 17:40:58 +0900

    퍼스트 스크린/백그라운드컬러 css(values셋팅)

---
+++ Android/app/src/main/java/com/example/android/firstscreen/firstscreenActivity.kt
@@ -0,0 +1,19 @@
+package com.example.android.firstscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.android.R
+import com.example.android.bbs.Bbs
+import com.example.android.signin.signinActivity
+
+class firstscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_firstscreen)
+
+
+
+    }
+}
