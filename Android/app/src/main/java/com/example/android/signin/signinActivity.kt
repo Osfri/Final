6e2c2589bb52259a97b6e37c7776d2f3a1fb7d17
jaehanNO <ssont86@gmail.com>
2022-03-16 17:11:16 +0900

    로그인

---
+++ Android/app/src/main/java/com/example/android/signin/signinActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.signin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android.R
+
+class signinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
