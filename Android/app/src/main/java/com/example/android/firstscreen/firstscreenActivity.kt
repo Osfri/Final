250954ba23ac1f261601ceb0c0ec2239cada2ecc
jaehanNO <ssont86@gmail.com>
2022-03-17 09:18:48 +0900

    회원가입(카카오/구글 로그인 미구현)

--- Android/app/src/main/java/com/example/android/firstscreen/firstscreenActivity.kt
+++ Android/app/src/main/java/com/example/android/firstscreen/firstscreenActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.android.R
-import com.example.android.bbs.Bbs
 import com.example.android.signin.signinActivity
 
 class firstscreenActivity : AppCompatActivity() {
@@ -13,8 +12,8 @@ class firstscreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_firstscreen)
 
-        val signin = findViewById<Button>(R.id.btn_signin)
-        val signup = findViewById<Button>(R.id.btn_signin)
+        val signin = findViewById<Button>(R.id.btn_signin1)
+        val signup = findViewById<Button>(R.id.btn_signin1)
 
         signin.setOnClickListener {
             val i = Intent(this,signinActivity::class.java)
