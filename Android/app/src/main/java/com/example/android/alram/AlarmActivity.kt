c9fa43ee63d2fe9240a016023c3f311490562929
charmPHJ <92033764+charmPHJ@users.noreply.github.com>
2022-03-23 10:48:10 +0900

    알람 년 월 일 설정 후 알람
    알람부분 수정

--- Android/app/src/main/java/com/example/android/alram/AlarmActivity.kt
+++ Android/app/src/main/java/com/example/android/alram/AlarmActivity.kt
@@ -1,15 +1,18 @@
 package com.example.android.alram
 
 import android.app.AlarmManager
+import android.app.NotificationManager
 import android.app.PendingIntent
-import android.app.TimePickerDialog
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.NotificationCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.android.R
@@ -19,25 +22,49 @@ import com.example.android.chat.ChatActivity
 import com.example.android.offday.OffDayActivity
 import com.example.android.pointMall.PointMallActivity
 import com.google.android.material.navigation.NavigationView
+import org.w3c.dom.Text
+import java.text.ParseException
+import java.text.SimpleDateFormat
 import java.util.*
+
 
 class AlarmActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener  {
 
     lateinit var navigationView: NavigationView
     lateinit var drawerLayout: DrawerLayout
 
+
+    private var alarmManager: AlarmManager? = null
+    private var mCalender: GregorianCalendar? = null
+
+    private var notificationManager: NotificationManager? = null
+    var builder: NotificationCompat.Builder? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm)
 
-        // 뷰를 초기화 해주기
-        initOnOffButton()
-        initChangeAlarmTimeButton()
 
-        // 저장된 데이터 가져오기
-        val model = fetchDataFromSharedPreferences()
-        // 뷰에 데이터를 그려주기
-        renderView(model)
+
+        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        mCalender = GregorianCalendar()
+
+        Log.v("AlarmActivity", mCalender!!.getTime().toString())
+
+        setContentView(R.layout.activity_alarm)
+
+        val ampmTextView = findViewById<TextView>(R.id.ampmTextView)
+        //접수일 알람 버튼
+        val onOffButton = findViewById<View>(R.id.onOffButton) as Button
+        onOffButton.setOnClickListener{
+
+            setAlarm()
+            Toast.makeText(this,"알람이 설정되었습니다",Toast.LENGTH_LONG).show()
+            ampmTextView.text = "알람이 곧 울립니다"
+        }
 
 
 
@@ -57,182 +84,30 @@ class AlarmActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
 
     }
 
+    private fun setAlarm(){
+        //AlarmReceiver에 값 전달
+        val receiverIntent = Intent(this@AlarmActivity, AlarmReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(this@AlarmActivity, 0, receiverIntent, 0)
 
-    private fun fetchDataFromSharedPreferences(): AlarmDisplayModel {
-        val sharedPreferences = getSharedPreferences(M_SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE)
+        //임의로 날짜와 시간을 지정 - 교대시간을 여기로 받아와야 할듯
+        val from = "2022-03-23 10:46:00"
 
-        // DB 에서 데이터 가져오기
-        val timeDBValue = sharedPreferences.getString(M_ALARM_KEY, "09:30") ?: "09:30"
-        val onOffDBValue = sharedPreferences.getBoolean(M_ONOFF_KEY, false)
-
-        // 시:분 형식으로 가져온 데이터 스플릿
-        val alarmData = timeDBValue.split(":")
-
-        val alarmModel = AlarmDisplayModel(alarmData[0].toInt(), alarmData[1].toInt(), onOffDBValue)
-
-        // 보정 조정 예외처 (브로드 캐스트 가져오기)
-        val pendingIntent = PendingIntent.getBroadcast(
-            this,
-            M_ALARM_REQUEST_CODE,
-            Intent(this, AlarmReceiver::class.java),
-            PendingIntent.FLAG_NO_CREATE
-        ) // 있으면 가져오고 없으면 안만든다. (null)
-
-        if ((pendingIntent == null) and alarmModel.onOff) {
-            //알람은 꺼져있는데, 데이터는 켜져있는 경우
-            alarmModel.onOff = false
-
-        } else if ((pendingIntent != null) and alarmModel.onOff.not()) {
-            // 알람은 켜져있는데 데이터는 꺼져있는 경우.
-            // 알람을 취소함
-            pendingIntent.cancel()
-        }
-        return alarmModel
-    }
-
-    // 시간 재설정 버튼.
-    private fun initChangeAlarmTimeButton() {
-        val changeAlarmButton = findViewById<Button>(R.id.changeAlarmTimeButton)
-
-
-
-        changeAlarmButton.setOnClickListener {
-            // 현재 시간을 가져오기 위해 캘린더 인스턴스
-            val calendar = Calendar.getInstance()
-            // TimePickDialog 띄워줘서 시간을 설정을 하게끔 하고, 그 시간을 가져와서
-            TimePickerDialog(this, { picker, hour, minute ->
-
-
-                // 데이터를 저장
-                val model = saveAlarmModel(hour, minute, false)
-                // 뷰를 업데이트
-                renderView(model)
-
-                // 기존에 있던 알람을 삭제한다.
-                cancelAlarm()
-
-            }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false)
-                .show()
-
-        }
-    }
-
-    // 알람 켜기 끄기 버튼.
-    private fun initOnOffButton() {
-        val onOffButton = findViewById<Button>(R.id.onOffButton)
-
-        print("initonoff")
-
-        onOffButton.setOnClickListener {
-            print("eeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
-            // 저장한 데이터를 확인한다
-            val model =
-                it.tag as? AlarmDisplayModel ?: return@setOnClickListener// 형변환 실패하는 경우에는 null
-            print("=========================" + model)
-            val newModel = saveAlarmModel(model.hour, model.minute, model.onOff.not()) // on off 스위칭
-            print("ddddddddddddddddddddddddd" + newModel)
-            renderView(newModel)
-
-            // 온/오프 에 따라 작업을 처리한다
-
-            if (newModel.onOff) {
-                // 온 -> 알람을 등록
-                val calender = Calendar.getInstance().apply {
-                    set(Calendar.HOUR_OF_DAY, newModel.hour)
-                    set(Calendar.MINUTE, newModel.minute)
-                    // 지나간 시간의 경우 다음날 알람으로 울리도록
-                    if (before(Calendar.getInstance())) {
-                        add(Calendar.DATE, 1) // 하루 더하기
-                    }
-                }
-
-                //알람 매니저 가져오기.
-                val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-                val intent = Intent(this, AlarmReceiver::class.java)
-                val pendingIntent = PendingIntent.getBroadcast(
-                    this,
-                    M_ALARM_REQUEST_CODE,
-                    intent,
-                    PendingIntent.FLAG_UPDATE_CURRENT
-                )
-                // 있으면 새로 만든거로 업데이트
-                // 잠자기 모드에서도 허용 하는 방법
-//                alarmManager.setAndAllowWhileIdle()
-//                alarmManager.setExactAndAllowWhileIdle()
-
-                alarmManager.setInexactRepeating( // 정시에 반복
-                    AlarmManager.RTC_WAKEUP, // RTC_WAKEUP : 실제 시간 기준으로 wakeup , ELAPSED_REALTIME_WAKEUP : 부팅 시간 기준으로 wakeup
-                    calender.timeInMillis, // 언제 알람이 발동할지.
-                    AlarmManager.INTERVAL_DAY, // 하루에 한번씩.
-                    pendingIntent
-                )
-                Toast.makeText(this@AlarmActivity,"알람이 등록되었습니다.",Toast.LENGTH_LONG).show()
-
-            } else {
-                // 오프 -> 알람을 제거
-                cancelAlarm()
-                Toast.makeText(this@AlarmActivity,"알람이 취소되었습니다.",Toast.LENGTH_LONG).show()
-
-
-            }
-        }
-    }
-
-    private fun renderView(model: AlarmDisplayModel) {
-        // 최초 실행 또는 시간 재설정 시 들어옴
-
-        findViewById<TextView>(R.id.ampmTextView).apply {
-            text = model.ampmText
-        }
-        findViewById<TextView>(R.id.timeTextView).apply {
-            text = model.timeText
-        }
-        findViewById<Button>(R.id.onOffButton).apply {
-            text = model.onOffText
-            tag = model
-        }
-    }
-
-    private fun cancelAlarm() {
-        // 기존에 있던 알람을 삭제한다.
-        val pendingIntent = PendingIntent.getBroadcast(
-            this,
-            M_ALARM_REQUEST_CODE,
-            Intent(this, AlarmReceiver::class.java),
-            PendingIntent.FLAG_NO_CREATE
-        ) // 있으면 가져오고 없으면 안만든다. (null)
-
-        pendingIntent?.cancel() // 기존 알람 삭제
-    }
-
-    private fun saveAlarmModel(hour: Int, minute: Int, onOff: Boolean): AlarmDisplayModel {
-        val model = AlarmDisplayModel(
-            hour = hour,
-            minute = minute,
-            onOff = onOff
-        )
-
-        // time 에 대한 db 파일 생성
-        val sharedPreferences = getSharedPreferences(M_SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE)
-
-        // edit 모드로 열어서 작업 (값 저장)
-        with(sharedPreferences.edit()) {
-            putString(M_ALARM_KEY, model.makeDataForDB())
-            putBoolean(M_ONOFF_KEY, model.onOff)
-            commit()
+        //날짜 포맷을 바꿔주는 소스코드
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        var datetime: Date? = null
+        try {
+            datetime = dateFormat.parse(from)
+        } catch (e: ParseException) {
+            e.printStackTrace()
         }
 
-        return model
+        val calendar = Calendar.getInstance()
+        calendar.time = datetime
+
+        alarmManager!![AlarmManager.RTC, calendar.timeInMillis] = pendingIntent
     }
 
 
-    companion object {
-        // static 영역 (상수 지정)
-        private const val M_SHARED_PREFERENCE_NAME = "time"
-        private const val M_ALARM_KEY = "alarm"
-        private const val M_ONOFF_KEY = "onOff"
-        private val M_ALARM_REQUEST_CODE = 1000
-    }
 
     // drawerlayout 네비바
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- Android/app/src/main/java/com/example/android/alram/AlarmReceiver.kt
+++ Android/app/src/main/java/com/example/android/alram/AlarmReceiver.kt
@@ -1,55 +1,80 @@
 package com.example.android.alram
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
+import android.R
+import android.app.*
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import com.example.android.R
+import com.example.android.MainActivity
+
 
 class AlarmReceiver : BroadcastReceiver() {
 
-    companion object {
-        const val NOTIFICATION_CHANNEL_ID = "1000"
-        const val NOTIFICATION_ID = 100
-    }
+    var manager: NotificationManager? = null
+    var builder: NotificationCompat.Builder? = null
+
+    //오레오 이상은 반드시 채널을 설정해줘야 Notification이 작동함
+    private val CHANNEL_ID = "channel1"
+    private val CHANNEL_NAME = "Channel1"
 
     override fun onReceive(context: Context, intent: Intent) {
+        val am = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-        // 채널 생성
-        createNotificationChannel(context)
-        // 알림
-        notifyNotification(context)
-
-    }
-
-    private fun createNotificationChannel(context: Context) {
-        // context : 실행하고 있는 앱의 상태나 맥락을 담고 있음
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                NOTIFICATION_CHANNEL_ID,
-                "onoff알람",
-                NotificationManager.IMPORTANCE_HIGH
+        builder = null
+        manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            manager!!.createNotificationChannel(
+                NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
             )
-
-            NotificationManagerCompat.from(context)
-                .createNotificationChannel(notificationChannel)
+            NotificationCompat.Builder(context, CHANNEL_ID)
+        } else {
+            NotificationCompat.Builder(context)
         }
+
+        //알림창 클릭 시 activity 화면 부름
+        val intent2 = Intent(context, AlarmActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(context, 101, intent2, PendingIntent.FLAG_UPDATE_CURRENT)
+
+
+        //알림창 제목
+        builder!!.setContentTitle("정해진 시간에 다 되었습니다")
+        //알림창 아이콘
+        builder!!.setSmallIcon(com.example.android.R.drawable.ic_launcher_background)
+        //알림창 터치시 자동 삭제
+        builder!!.setAutoCancel(true)
+        builder!!.setContentIntent(pendingIntent)
+        val notification: Notification = builder!!.build()
+        manager!!.notify(1, notification)
     }
-
-    private fun notifyNotification(context: Context) {
-        with(NotificationManagerCompat.from(context)) {
-            val build = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
-                .setContentTitle("알람")
-                .setContentText("설정하신 시간이 되었습니다")
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-
-            notify(NOTIFICATION_ID, build.build())
-        }
-    }
-
 }
+
+/*
+
+private fun createNotificationChannel(context: Context) {
+    // context : 실행하고 있는 앱의 상태나 맥락을 담고 있음
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val notificationChannel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            "onoff알람",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+
+        NotificationManagerCompat.from(context)
+            .createNotificationChannel(notificationChannel)
+    }
+}
+
+private fun notifyNotification(context: Context) {
+    with(NotificationManagerCompat.from(context)) {
+        val build = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setContentTitle("알람")
+            .setContentText("설정하신 시간이 되었습니다")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+
+        notify(NOTIFICATION_ID, build.build())
+    }
+}
+*/
